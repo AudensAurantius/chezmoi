263277c3719c0d76e8898c20d81630751a305a0d
Michael Haynes <mhaynes@boldorange.com>
2024-06-18 12:04:36 -0500

    add nvim config files

---
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,6 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- open file explorer
+vim.keymap.set("n", "<leader>o", ":Ranger<CR>")
